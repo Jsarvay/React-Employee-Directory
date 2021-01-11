815a86f6f2cf646ab0af44f3d7b6d4ee27006d09
Kilroy <jordans1@vt.edu>
2021-01-11 17:00:18 -0500

    Added working api route and populate employee component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ import API from "./util/API";
 
 class App extends Component {
   state = {
+    search: "",
     employee: []
+  };
+
+  componentDidMount() {
+    API.getRandomEmployee()
+    .then(res => this.setState({ employee: res.data.results }))
+    .catch(err => console.log(err));
+    console.log(this.state.employee)
   };
 
 
@@ -17,7 +25,17 @@ class App extends Component {
       <Wrapper>
         <Header />
         <Search />
-        <Employees />
+        {this.state.employee.map(emp => (
+          <Employees 
+          key={emp.id.value}
+          firstname={emp.name.first}
+          lastname={emp.name.last}
+          email={emp.email}
+          phone={emp.phone}
+          dob={emp.dob.date}
+          image={emp.picture.thumbnail}
+          />
+        ))}
       </Wrapper>
   );
   }
--- src/components/Employees/Employees.js
+++ src/components/Employees/Employees.js
@@ -3,8 +3,21 @@ import "./style.css";
 
 function Employees(props) {
     return (
-        <div>
+        <div className="card-wrap">
+            <div className="card">
+                <div className="card-image">
+                    <img alt={props.firstname} src={props.image}></img>
+                </div>
+                <div className="content">
+                    <ul>
+                        <li><strong>Name: </strong>{props.firstname} {props.lastname}</li> 
+                        <li><strong>Email: </strong> {props.email}</li> 
+                        <li><strong>Phone: </strong> {props.phone}</li> 
+                        <li><strong>Date of Birth: </strong> {props.dob}</li>
+                    </ul>
+                </div>
 
+           </div>
         </div>
     )
 };
